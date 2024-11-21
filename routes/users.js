517950cd118ce31ab0936b92ba72ff6eeb6a8a41
NicolasJoubert <nicolasjoubert@icloud.com>
2024-11-22 00:14:12 +0100

    maj back

--- routes/users.js
+++ routes/users.js
@@ -44,11 +44,11 @@ router.post("/signin", (req, res) => {
 
   User.findOne({ username: req.body.username }).then((data) => {
     if (data && bcrypt.compareSync(req.body.password, data.password)) {
-      console.log(data)
-      res.json({ 
-        result: true, 
+      console.log(data);
+      res.json({
+        result: true,
         token: data.token,
-        firstname: data.firstname
+        firstname: data.firstname,
       });
     } else {
       res.json({ result: false, error: "User not found or wrong password" });
