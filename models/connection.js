911eb44e4ccc82d93847de71001659dfe689e520
NicolasJoubert <nicolasjoubert@icloud.com>
2024-11-21 12:33:42 +0100

    database connected

--- models/connection.js
+++ models/connection.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
 
-const connectionString = "mongodb+srv://.../todoapp";
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose
   .connect(connectionString, { connectTimeoutMS: 2000 })
   .then(() => console.log("Database connected"))
-
   .catch((error) => console.error(error));
