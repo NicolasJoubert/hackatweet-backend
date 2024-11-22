dcf907be968ad64ee01371b48b72d0a03b435f69
benKapl <benji1.kaplan@gmail.com>
2024-11-22 15:03:58 +0100

    create route to add or update hashtag

--- routes/hashtags.js
+++ routes/hashtags.js
@@ -15,26 +15,51 @@ router.post("/", async (req, res) => {
 
   try {
     const { hashtagContent, tweetContent, username } = req.body
-    // check that hashtag exists
-    const hashtag = await Hashtag.findOne( {content: hashtagContent})
-
-    if (!hashtag) { // create hashtag in DB
-
-        // get user who posted tweet with hashtag
-        const user = await User.findOne({ username });
-        if (!user) {
-            res.json({ result: false, error: "User not found" });
-            return;
-        }
-
-        // get tweet
-        const tweet = await Tweet.findOne({ content: tweetContent, user: user._id });
-        if (!tweet) {
-            res.json({ result: false, error: "could not find tweet" });
-            return;
-        }
-        console.log(tweet)
+    
+    // get user who posted tweet with hashtag
+    const user = await User.findOne({ username });
+    if (!user) {
+        res.json({ result: false, error: "User not found" });
+        return;
     }
+    
+    // get tweet
+    const tweet = await Tweet.findOne({ content: tweetContent, user: user._id });
+    console.log(tweet)
+    if (!tweet) {
+        res.json({ result: false, error: "could not find tweet" });
+        return;
+    }
+    
+    // manage hashtag creation or update
+    const hashtag = await Hashtag.findOne( {content: hashtagContent})
+    if (!hashtag) {
+        // create new hashtag
+        const newHashtag = new Hashtag({
+            content: hashtagContent,
+            tweets: [tweet._id]
+        })
+        const savedHashtag = await newHashtag.save()
+        if (!savedHashtag) {
+            res.json({ result: false, error: "could not save hashtag" });
+            return;
+        }
+        res.json({result: true})
+    } else {
+        // update hashtag with new tweet
+        const updatedHashtag = await Hashtag.updateOne(
+            { content: hashtagContent },
+            { $push: { tweets: tweet._id } }
+        )
+        if (updatedHashtag.modifiedCount === 0) {
+            res.json({ result: false, error: "could not update" });
+            return;
+        }
+
+        res.json({ result: true })
+    }
+    
+
 
 
   } catch(err) {
