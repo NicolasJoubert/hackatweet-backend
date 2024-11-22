d313a3343c1ab673a37a0290f2f5be6e9c38842c
NicolasJoubert <nicolasjoubert@icloud.com>
2024-11-22 17:00:54 +0100

    sort hashtag

--- routes/hashtags.js
+++ routes/hashtags.js
@@ -6,22 +6,22 @@ const User = require("../models/users");
 const Tweet = require("../models/tweets");
 const Hashtag = require("../models/hashtags");
 
-
 router.get("/", async (req, res) => {
-    const hashtags = await Hashtag.find();
-    if (hashtags.length == 0) {
-        res.json({ result: false, error: "no hashtag in db"})
-        return
-    }
+  const hashtags = await Hashtag.find();
+  if (hashtags.length == 0) {
+    res.json({ result: false, error: "no hashtag in db" });
+    return;
+  }
 
-    const hashtaglist = hashtags.map((hashtag) => {
-        return {
-            content: hashtag.content,
-            tweetsNumber: hashtag.tweets.length
-        }
-    })
+  const hashtaglist = hashtags.map((hashtag) => {
+    return {
+      content: hashtag.content,
+      tweetsNumber: hashtag.tweets.length,
+    };
+  });
+  hashtaglist.sort((a, b) => b.tweetsNumber - a.tweetsNumber);
 
-    res.json({ result: true, hashtags: hashtaglist })
-})
+  res.json({ result: true, hashtags: hashtaglist });
+});
 
 module.exports = router;
